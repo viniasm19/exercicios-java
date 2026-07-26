2e2a16f406f66a6e7a6b944fa65990a46610bdc7
Vinicius Melo <Vinicius Melo>
2026-07-26 15:11:22 -0300

    adicionando mais exercicios

---
+++ Casting.java
@@ -0,0 +1,9 @@
+public class Casting {
+    public static void main (String[] args) {
+        double numeroDouble = 8.9;
+        int numeroInteiro = (int) numeroDouble;
+
+        System.out.println("Variavel double: " + numeroDouble);
+        System.out.println("Casting double para int: " + numeroInteiro);
+    }
+}
---
+++ DesafioMedia.java
@@ -0,0 +1,11 @@
+public class DesafioMedia {
+    public static void main (String[] args) {
+        double nota1 = 9.3;
+        double nota2 = 8.7;
+        double media = (nota1 + nota2) /2;
+
+        System.out.println("Nota 1: " + nota1);
+        System.out.println("Nota 2: " + nota2);
+        System.out.println("Média: " + media);
+    }
+}
